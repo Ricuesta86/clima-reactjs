f5a87d87e1f28dd61d5ebe23b864b50a25bb6d3d
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-23 12:22:11 -0500

    Se creo el encabezado de la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
 
 class App extends Component {
   render(){
         return (
-          <div className="App">
-              hola desde a
+          <div className="app">
+              <Header 
+                titulo="Aplicacion del Clima"
+              />
           </div>
         );
   }
