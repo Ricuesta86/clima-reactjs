c7534b891dc432a33d2d61f8b4a70d40311f79a0
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-23 14:26:05 -0500

    Tomamos los datos desde el formulario de ciudad y pais

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
 class Formulario extends Component {
+
+    ciudadRef=React.createRef();
+    paisRef=React.createRef();
+
+
+    buscarClima = (e) => {
+        e.preventDefault();
+
+        // leer los ref y crear los objetos
+        const respuesta={
+            ciudad: this.ciudadRef.current.value,
+            pais: this.paisRef.current.value
+        }
+        console.log(respuesta);
+        
+    }
+
     render() {
         return (
             <div className="contenedor-form">
                 <div className="container">
                     <div className="row">
-                        <form>
+                        <form onSubmit={this.buscarClima}> 
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <input type="text" id="ciudad"/>
+                                <input ref={this.ciudadRef} type="text" id="ciudad"/>
                                 <label htmlFor="ciudad">Ciudad:</label>
                             </div>
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <select id="pais">
+                                <select id="pais" ref={this.paisRef}>
                                     <option value="" defaultValue>Escoge un País</option>
                                     <option value="AR">Argentina</option>
                                     <option value="CO">Colombia</option>
