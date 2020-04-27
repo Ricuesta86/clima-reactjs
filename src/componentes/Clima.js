74951f2e4bc4119cb18f38c83587b8d59a8e0282
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-27 17:45:41 -0500

    Mostrando resultados en el componente clima

--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 class Clima extends Component {
+
+    mostraResultado=()=>{
+
+        const {name, weather, main}=this.props.resultado;
+
+        if( !name || !weather || !main)
+            return null;
+
+        return(
+            <h1>{name}</h1>
+        );
+    }
     
     render() {
 
-        console.log(this.props.resultado);
+        console.log(this.props.resultado );
     
         return (
             <div className="container">
-                desde clima.js
+                {this.mostraResultado()}
             </div>
         );
     }
